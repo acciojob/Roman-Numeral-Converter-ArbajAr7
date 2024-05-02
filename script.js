c11c8b8d1035a1b12e8eab2e06cc961c96eaa1e3
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-03 00:04:17 +0545

    created a submission

--- script.js
+++ script.js
@@ -10,26 +10,10 @@ function convertToRoman(num) {
     };
 
   //your code here
-	let m = ["", "M", "MM", "MMM"];
-    let c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-    let x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-    let i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-        if(num<4000)
-        {
-            let thousands = m[parseInt(num/1000)];
-            let hundreds = c[parseInt((num%1000) / 100)];
-            let tens = x[parseInt((num%100) / 10)];
-            let ones = i[parseInt(num%10)];
-                
-            let ans = thousands + hundreds + tens + ones;
-            // console.log(ans);
-            return ans;
-        }
-
-}
+	
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+// console.log(convertToRoman(36));
 
 
 
